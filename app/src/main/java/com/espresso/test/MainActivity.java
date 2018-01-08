9d211ce73a7bd6eb3a4318020f4f2a03edf186d3
Amit Siddhpura <amit.siddhpura@infostretch.com>
2018-01-08 10:55:25 +0530

    first commit

---
+++ app/src/main/java/com/espresso/test/MainActivity.java
@@ -0,0 +1,35 @@
+package com.espresso.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity  extends Activity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.inputField);
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.changeText:
+                editText.setText("Amit Siddhpura");
+                break;
+            case R.id.switchActivity:
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("input", editText.getText().toString());
+                startActivity(intent);
+                break;
+        }
+
+    }
+}
